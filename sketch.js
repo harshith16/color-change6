b891b6e776d01b3ea1981f85f19e56341e6fc949
harshith16 <87561540+harshith16@users.noreply.github.com>
2021-07-19 12:12:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+var box
+function setup() {
+  createCanvas(400,400);
+box=createSprite(200,200,50,50);
+
+
+}
+
+function draw() 
+{
+  background("blue");
+if (keyDown("right")){
+box.x=box.x+5
+background("red"); 
+}
+if (keyDown("down")){
+box.y=box.y+6
+background("yellow");
+}
+
+
+  drawSprites()
+}
+
+
+
+
